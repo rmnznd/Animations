5ac8e4fc564c93b6a1633b339c2f7df7c7ccb87b
Ramin <rmnznd@gmail.com>
2025-07-31 23:53:39 -0700

    Create animated text

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    let letters = Array("Hello SwiftUI")
+    
+    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+     
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count, id: \.self) {num in
+                Text(String(letters[num]))
+                    .padding(5)
+                    .font(.title)
+                    .background(enabled ? .blue : .red)
+                    .offset(dragAmount)
+                    .animation(.linear.delay(Double(num) / 20), value: dragAmount)
+            }
         }
-        .padding()
+        .gesture(
+            DragGesture()
+                .onChanged {dragAmount = $0.translation
+                }
+                .onEnded {_ in 
+                    dragAmount = .zero
+                    enabled.toggle()
+                }
+        )
     }
 }
 
